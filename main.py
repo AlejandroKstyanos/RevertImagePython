deeaa62d533c6ad9222934c783d9e6c2080c8c6d
AlejandroKstyanos <1930420@upv.edu.mx>
2022-03-25 08:27:10 -0600

    Added more assets and changing example code.

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def inverter(image):
     for u in range(w):
         for v in range(h):
             p = image[u,v] # Obtiene el pixel
-            image[u,v] = 255 - p # inverte su valor restando 255 menos el valor original
+            image[u,v] = 255 - p # invierte su valor restando 255 menos el valor original
 
     return image
 
@@ -17,12 +17,14 @@ def inverter(image):
 if __name__ == '__main__':
 
     img1 = cv2.imread('assets/1.jpeg')
-    img2 = cv2.imread('assets/2.jpg')
+    img2 = cv2.imread('assets/2.png')
+    img2 = cv2.resize(img2,None,fx=0.5,fy=0.5, interpolation=cv2.INTER_CUBIC)
 
     cv2.imshow('Imagen 1', img1)
     cv2.imshow('Imagen 2', img2)
     
     img1 = inverter(img1)
+    img2 = inverter(img2)
     cv2.imshow('Image Inverted 1', img1)
     cv2.imshow('Image Inverted 2', img2)
     # print(img1,'\n')
